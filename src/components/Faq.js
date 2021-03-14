990171e60fae94742f6b9137bff33ac3b87878a9
kenneth_crasta <kenneth.crasta@oracle.com>
2021-03-14 01:51:12 -0500

    v1

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -18,10 +18,36 @@ const Dl = styled.dl`
 const faqs = [
   {
     question:
-      'Is Drive Test Pro the best school in Ajax?',
+      'What do I need before I can start having driving lessons with Drive Test Pro?',
     answer:
-      'Yes it is the BEST.'
-  }
+      'You must have a G1 or G2. You can register on line or over the phone or come\n' +
+        '\n' +
+        'to the office saturday or sunday from 9am to 2:15pm.'
+  },
+    {
+        question:
+            'How much discount will I get on my insurance with the certificate?',
+        answer:
+            'Our students are saving up to 41% annually, may vary depending insurance companies, your age, type of the car, your driving record etc.'
+    },
+    {
+        question:
+            'How will I know if i\'m ready to take my driving exam?',
+        answer:
+            'Your instructor will give you a mock driving test, and advise you accordingly. Students ready for the driving test should be able to drive consistently well and with confidence – without assistance and guidance. Ninety per cent of test failures are due to inexperience in traffic.'
+    },
+    {
+        question:
+            'What is the current waiting time for practical driving test appointments?',
+        answer:
+            'This depends on where you live in. In some areas, it’s only 3 weeks to 4 weeks.'
+    },
+    {
+        question:
+            'What is the current fee for practical driving test?',
+        answer:
+            'Class G2 road test $50 -Class G road test $85 payable to the Ministry Of Transportation.'
+    }
 ];
 
 export default () => {
