d79d5931cc851c9686da0ade6002ce3d50ebf54f
Kenneth John <kensplanet@yahoo.com>
2021-03-27 01:23:15 -0400

    Remove fake info - V1.0.0 alpha.6 (#6)

--- src/components/Rate.js
+++ src/components/Rate.js
@@ -2,20 +2,28 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
-import {Box} from "@material-ui/core";
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        marginTop: '100px',
+        marginTop: '20px',
         marginLeft: '20px',
         marginRight: '20px'
     },
-    margin: {
-        height: theme.spacing(3),
-    },
     rate: {
         marginTop: '30px'
-    }
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 120,
+        marginBottom: '60px'
+    },
+    selectEmpty: {
+        marginTop: theme.spacing(2),
+    },
 }));
 
 const marks = [
@@ -46,32 +54,39 @@ function debounce(func, wait, immediate) {
     };
 }
 
-
 export default function DiscreteSlider() {
     const classes = useStyles();
 
-    const [cost, setCost] = React.useState(90);
+    const [cost, setCost] = React.useState(80);
     const [value, setValue] = React.useState(2);
+    const [category, setCategory] = React.useState('G2');
+
+    const handleCategoryChange = (event) => {
+        setCategory(event.target.value);
+        let rate = (event.target.value === 'G2') ? 40 : 45;
+        setCost(rate * value);
+    };
 
     const handleChange = (event, number) => {
         setValue(number);
-        let rate = 45, totalCost = 0;
-        let getRate = (n) => {
-            if (n <=5 ) { rate = 45; return rate;}
-            if (n <=10 ) { rate = 40; return rate;}
-            if (n <=15 ) { rate = 35; return rate;}
-            if (n <=20 ) { rate = 30; return rate;}
-            return rate;
-        };
-
-        for (let i = 1; i <= number; i++) {
-            totalCost += getRate(i);
-        }
-        setCost(totalCost);
+        let rate = (category === 'G2') ? 40 : 45;
+        setCost(rate * number);
     };
 
     return (
         <div className={classes.root}>
+            <FormControl variant="filled" className={classes.formControl}>
+                <InputLabel id="demo-simple-select-filled-label">Class</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={category}
+                    onChange={handleCategoryChange}
+                >
+                    <MenuItem value={'G2'}>G2 (40$/hour)</MenuItem>
+                    <MenuItem value={'G'}>G (45$/hour)</MenuItem>
+                </Select>
+            </FormControl>
             <Slider
                 defaultValue={2}
                 getAriaValueText={valuetext}
@@ -82,7 +97,7 @@ export default function DiscreteSlider() {
                 valueLabelDisplay="on"
             />
             <Typography variant="h3" gutterBottom className={classes.rate}>
-                We'll charge ${cost} for {value} hours
+                We'll charge ${cost} for {value} hours ({category} Class)
             </Typography>
         </div>
     );
